0fbae759ed6e68c0d912bbaced13c4a4780414a5
nikosgalanis <ngalanis99@gmail.com>
2019-12-17 20:43:41 +0200

    Split_data_block ready and better than ever!

--- include/insert_functions.h
+++ include/insert_functions.h
@@ -8,10 +8,8 @@
 #include "./util.h"
 
 /* Returns a string which contains exactly the info we want to append
-  to the higher level block. Also, it returns the allocated blocks's number
-  by passing its value to the new_block_num pointer argument */
-char* split_data_block(int fileDesc, int block_num, Record* new_record,
-                       int* new_block_num);
+  to the higher level block. */
+char* split_data_block(int fileDesc, int block_num, Record* new_record);
 /* Crates the root of B+ tree and returns its block index */
 int create_root(int fileDesc, void* key);
 
