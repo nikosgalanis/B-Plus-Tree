5336fef280fd0afbabf6163d81fd09dbc0f1b0e8
Sofo <46571975+SofoSt@users.noreply.github.com>
2019-12-21 02:01:45 +0200

    print in util.h

--- include/util.h
+++ include/util.h
@@ -96,3 +96,6 @@ int find_index(int fileDesc);
 int find_scan(int fileDesc);
 char* concat(const char *s1, const char *s2);
 int compare(void *op1, int op, void *op2, char type);
+boolean print_data_block(int fileDesc, int target_block); 
+boolean print_index_block(int fileDesc, int target_block);
+boolean print_tree(int fileDesc, char print_type);
